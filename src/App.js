81c2eca3b208510f148c210c37ae7c437aae2172
fucusifei <spelpog@gmail.com>
2023-01-31 09:53:32 +0200

    init end calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import {makeStyles} from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      parameter: '',
+      mark:'',
+      out: '0',
+      container: '',
+      arr : []
+    }
+    this.ref = React.createRef()
+  }
+
+  pressNumberButton(number) {
+      this.setState((state) => {
+          if(state.out === '0') {state.out = ''}
+          state.container = state.container + number
+          return {out: state.out + number}
+      });
+
+  }
+  pressMarkButton(mark) {
+      this.setState((state) => {
+          if(state.out === '0')
+          {
+              return  state.out
+          }
+          else {
+              if (state.mark === ''){
+                  state.parameter = state.container
+                  state.container = ''
+                  state.mark = mark
+                  return {out: state.out + mark}
+              }
+              else {
+                  state.out = state.parameter
+                  state.mark = mark
+                  return {out: state.out + mark}
+              }
+
+          }
+      });
+    }
+
+  pressEqualButton() {
+      this.setState((state) => {
+          if(state.out === '0' || state.mark === '' || state.container === '')
+          {
+              return  state.out
+          }
+          else {
+              switch (state.mark) {
+                  case '+' :
+                      state.container = parseFloat(state.parameter) + parseFloat(state.container)
+                      state.mark = ''
+                      break
+                  case '-' :
+                      state.container = parseFloat(state.parameter) - parseFloat(state.container)
+                      state.mark = ''
+                      break
+                  case '*' :
+                      state.container = parseFloat(state.parameter) * parseFloat(state.container)
+                      state.mark = ''
+                      break
+                  case '/' :
+                      if (state.container === '0'){
+                          state.arr.push(state.out + " = Error division by zero")
+                          state.mark = ''
+                          return {out: state.container}
+                      }
+                      else {
+                          state.container = parseFloat(state.parameter) / parseFloat(state.container)
+                          state.mark = ''
+                      }
+                      break
+              }
+              state.arr.push(state.out + " = " + state.container)
+              return {out: state.container}
+          }
+      });
+  }
+
+
+  render() {
+    const {
+        out,
+        arr
+    } = this.state;
+
+
+
+    return (
+        <div>
+            <div>
+                {out}
+            </div>
+          <div>
+            <Button variant="contained" onClick={() => this.pressNumberButton('1')}>1</Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('2')}>2</Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('3')}>3</Button>
+            <Button variant="contained" onClick={() => this.pressMarkButton('+')}>+</Button>
+          </div>
+          <div>
+            <Button variant="contained" onClick={() => this.pressNumberButton('4')}>4</Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('5')}>5</Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('6')}>6</Button>
+            <Button variant="contained" onClick={() => this.pressMarkButton('-')}>-</Button>
+          </div>
+          <div>
+            <Button variant="contained" onClick={() => this.pressNumberButton('7')}>7</Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('8')}>8</Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('9')}>9</Button>
+            <Button variant="contained" onClick={() => this.pressMarkButton('*')}>*</Button>
+          </div>
+            <div>
+            <Button variant="contained" > </Button>
+            <Button variant="contained" onClick={() => this.pressNumberButton('0')}>0</Button>
+            <Button variant="contained" onClick={() => this.pressEqualButton()}>=</Button>
+            <Button variant="contained" onClick={() => this.pressMarkButton('/')}>/</Button>
+            </div>
+            <div>
+                {arr.map(name => <li key={name}> {name} </li>)}
+            </div>
+        </div>
+
+
+    );
+  }
 }
 
 export default App;
